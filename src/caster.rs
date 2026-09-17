0a16d93f17ea6f2782de23073c9c6bfec747d557
Oscar Rompich <oscarligo6@gmail.com>
2026-09-17 13:16:22 -0600

    feat: z-buffer implementation

--- src/caster.rs
+++ src/caster.rs
@@ -1,25 +1,24 @@
 use glam::Vec3A;
 use raylib::prelude::Color;
-
-#[derive(Clone, Debug, Copy)]
-pub struct Material {
-    pub diffuse: Color,
-    pub specular: Color,
-}
+use crate::material::Material;
 
 #[derive(Debug, Clone, Copy)]
 #[allow(dead_code)]
 pub struct Intersection {
     pub distance: f32,
+    pub point: Vec3A,
+    pub normal: Vec3A,
     pub is_intersecting: bool,
     pub material: Material,
 }
 
 
 impl Intersection {
-    pub fn new(distance: f32, is_intersecting: bool, material: Material) -> Self {
+    pub fn new(distance: f32, point: Vec3A, normal: Vec3A, is_intersecting: bool, material: Material) -> Self {
         Self {
             distance,
+            point,
+            normal,
             is_intersecting,
             material,
         }
@@ -28,11 +27,10 @@ impl Intersection {
     pub fn no_intersection() -> Self {
         Self {
             distance: f32::INFINITY,
+            point: Vec3A::ZERO,
+            normal: Vec3A::ZERO,
             is_intersecting: false,
-            material: Material {
-                diffuse: Color::BLACK,
-                specular: Color::BLACK,
-            },
+            material: Material::no_intersection(),
         }
     }
 }
@@ -52,13 +50,27 @@ pub fn cast_ray(
 
 ) -> Color {
 
+    let mut intersection = Intersection::no_intersection();
+    let mut z_buffer = f32::INFINITY;
+
     for object in objects {
-        let intersection = object.intersect(ray);
-        if intersection.is_intersecting {
-            return intersection.material.diffuse;
+        let tmp_intersection = object.intersect(ray);
+        if tmp_intersection.is_intersecting && tmp_intersection.distance < z_buffer {
+            intersection = tmp_intersection;
+            z_buffer = tmp_intersection.distance;
         }
     }
 
-    Color::BLACK 
+    if !intersection.is_intersecting {
+        return Color::BLACK;
+    } 
+
+    return Color::new(
+        (intersection.material.diffuse_color.x * 255.0) as u8,
+        (intersection.material.diffuse_color.y * 255.0) as u8,
+        (intersection.material.diffuse_color.z * 255.0) as u8,
+        255,
+    );
+
     
 }
